7946d3b05b9baecebc7fff2523a3c577fe67fd8a
Oleg O <oleg_biblyi@epam.com>
2022-08-15 10:06:03 +0300

    init supabase service

---
+++ src/app/core/services/supabase-auth.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SupabaseAuthService {
+  constructor(private http: HttpClient) {}
+
+  login() {
+    const URL = environment.supabase.URL + '/auth/v1/token?grant_type=password';
+    const email = '';
+    const password = '';
+
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        apikey: environment.supabase.PUBLIC_KEY,
+      }),
+    };
+
+    return this.http.post(URL, { email, password }, httpOptions);
+  }
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,12 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  supabase: {
+    URL: 'https://pkfelkgzmqeaywxfjiyq.supabase.co',
+    PUBLIC_KEY:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBrZmVsa2d6bXFlYXl3eGZqaXlxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTg0OTQ5OTAsImV4cCI6MTk3NDA3MDk5MH0.EeWH9v64OWEesnbttavF9WhgAdzOkxAfmTEDI5zSTfA',
+  },
 };
 
 /*
