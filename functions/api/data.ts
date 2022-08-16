612cfd7cb4aa8d49dd17eb963807d3afa20554c2
Oleg O <oleg_biblyi@epam.com>
2022-08-16 19:09:32 +0300

    init data base connection

--- functions/api/data.ts
+++ functions/api/data.ts
@@ -1,3 +1,5 @@
+import { PrismaClient } from '@prisma/client/edge';
+
 export async function onRequest(
   context: EventPluginContext<any, any, any, any>
 ) {
@@ -10,12 +12,47 @@ export async function onRequest(
     data, // arbitrary space for passing data between middlewares
   } = context;
 
-  const payload = {
-    hello: 'My First Function',
-    token: true,
-    verified: true,
-  };
-  const json = JSON.stringify(payload, null, 2);
+  const prisma = new PrismaClient({
+    datasources: {
+      db: {
+        url: env.DATABASE_URL,
+      },
+    },
+  });
 
-  return new Response(json);
+  // waitUntil method is used for sending logs, after response is sent
+  waitUntil(
+    prisma.user.create({
+      data: {
+        name: 'Alex Main',
+        email: 'test@gmail.com,',
+      },
+    })
+  );
+
+  console.log(env);
+  return new Response(`Hi`);
 }
+
+// addEventListener('fetch', (event) => {
+//   event.respondWith(handleEvent(event))
+// })
+
+// async function handleEvent(event: FetchEvent): Promise<Response> {
+//   const { request } = event
+
+// // waitUntil method is used for sending logs, after response is sent
+// event.waitUntil(
+//   prisma.log.create({
+//     data: {
+//       level: 'Info',
+//       message: `${request.method} ${request.url}`,
+//       meta: {
+//         headers: JSON.stringify(request.headers),
+//       },
+//     },
+//   })
+// )
+
+//   return new Response(`request method: ${request.method}!`)
+// }
