79331df3bfd56b8c51c260daae1cd21d5ac0bc81
Oleg O <oleg_biblyi@epam.com>
2022-08-19 11:55:13 +0300

    test vars prisma example

--- functions/api/data.ts
+++ functions/api/data.ts
@@ -1,38 +1,12 @@
-// export async function onRequest(context: EventPluginContext<any, any, any, any>) {
-//   const {
-//     request, // same as existing Worker API
-//     env, // same as existing Worker API
-//     params, // if filename includes [id] or [[path]]
-//     waitUntil, // same as ctx.waitUntil in existing Worker API
-//     next, // used for middleware or to fetch assets
-//     data, // arbitrary space for passing data between middlewares
-//   } = context;
-
-//   const data1 = {
-//     hello: 'My First Function',
-//     token: true,
-//     verified: true,
-//   };
-//   const json = JSON.stringify(data1, null, 2);
-
-//   return new Response('HELLO');
-// }
-
-
 import { PrismaClient } from '@prisma/client/edge';
 
 export async function onRequest(
   context: EventPluginContext<any, any, any, any>
 ) {
-  const {
-    request, // same as existing Worker API
-    env, // same as existing Worker API
-    params, // if filename includes [id] or [[path]]
-    waitUntil, // same as ctx.waitUntil in existing Worker API
-    next, // used for middleware or to fetch assets
-    data, // arbitrary space for passing data between middlewares
-  } = context;
-  // const prisma = new PrismaClient()
+  const { env } = context;
+
+  console.log('db url', env.DATABASE_URL);
+  
   const prisma = new PrismaClient({
     datasources: {
       db: {
@@ -41,7 +15,6 @@ export async function onRequest(
     },
   });
 
-  // // waitUntil method is used for sending logs, after response is sent
   // waitUntil(
   //   prisma.user.create({
   //     data: {
@@ -50,13 +23,6 @@ export async function onRequest(
   //     },
   //   })
   // );
-  const data1 = {
-    hello: 'My First Function',
-    token: true,
-    verified: true,
-    bla: env
-  };
-  const json = JSON.stringify(data1, null, 2);
 
-  return new Response(json);
+  return new Response('Success');
 }
--- src/app/core/services/supabase-auth.service.ts
+++ src/app/core/services/supabase-auth.service.ts
@@ -1,6 +1,5 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -12,19 +11,4 @@ export class SupabaseAuthService {
     const url = `/api/data`;
     return this.http.get<any>(url);
   }
-
-  // login() {
-  //   const URL = environment.supabase.URL + '/auth/v1/token?grant_type=password';
-  //   const email = '';
-  //   const password = '';
-
-  //   const httpOptions = {
-  //     headers: new HttpHeaders({
-  //       'Content-Type': 'application/json',
-  //       apikey: environment.supabase.PUBLIC_KEY,
-  //     }),
-  //   };
-
-  //   return this.http.post(URL, { email, password }, httpOptions);
-  // }
 }
